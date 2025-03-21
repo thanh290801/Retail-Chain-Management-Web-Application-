8252c1d1a5d9c940fb0b6b9e90eff2fda20a4134
trungpd <105336703+trungdepzaii@users.noreply.github.com>
2025-03-21 20:57:45 +0700

    .

--- retail-chain-management-backend/RCM.Backend/RCM.Backend/obj/Debug/net8.0/RCM.Backend.AssemblyInfo.cs
+++ retail-chain-management-backend/RCM.Backend/RCM.Backend/obj/Debug/net8.0/RCM.Backend.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a83e53306e02f8234895c663c4ec511b009986c")]
+
 [assembly: System.Reflection.AssemblyProductAttribute("RCM.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RCM.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
