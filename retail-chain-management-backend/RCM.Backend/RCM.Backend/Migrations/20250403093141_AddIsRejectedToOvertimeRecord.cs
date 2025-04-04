3021fa0860459d61116cf82edb8faf508c848a8a
TrungPD9 <trungpdhe153469@fpt.edu.vn>
2025-04-04 12:31:35 +0700

    update

---
+++ retail-chain-management-backend/RCM.Backend/RCM.Backend/Migrations/20250403093141_AddIsRejectedToOvertimeRecord.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RCM.Backend.Migrations
+{
+    public partial class AddIsRejectedToOvertimeRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRejected", // Tên cột mới
+                table: "OvertimeRecords", // Tên bảng hiện có
+                type: "bit", // Kiểu dữ liệu trong SQL Server (boolean)
+                nullable: false, // Không cho phép null
+                defaultValue: false); // Giá trị mặc định là false
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRejected",
+                table: "OvertimeRecords");
+        }
+    }
+}
